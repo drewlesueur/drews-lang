49ee29f80b3fbd8c2aad25a1156f793a1e36e9a4
drewlesueur <drewalex@gmail.com>
2013-02-26 03:18:16 +0000

    updates

--- drews_lang.js
+++ drews_lang.js
@@ -1,5 +1,6 @@
 // Possible todo:
 // allow to push and pop the code too.
+//implement map
 poor_module("drews_lang", function () {
   var to_string = Object.prototype.toString
   var is_array = function (a) { return to_string.call(a) == '[object Array]' }
@@ -23,7 +24,6 @@ poor_module("drews_lang", function () {
   }
 
   var call_cmd = function (name, args) {
-    commands.push_stack()
     //doing "i" and "scope" here
     i_stack.push(i)
     var old_scope = scope
@@ -34,108 +34,70 @@ poor_module("drews_lang", function () {
     scope.args = args
   }
 
+  var line = [];
+  var code;
+  var code_stack = [];
+  var i = 0;
+  var i_stack = [];
+  var scope = {};
+  var scope_stack = {}; //or use scope.parent_scope?
+  var fn_map = {}
+  var ret_val;
+  var raw_get = function (name) {
+    if (name[0] == "'") {
+      return name.substr(1);
+    } else {
+      return scope[name];
+    }
+  }
+  var get_args = function (args, start) {
+    ret = [];
+    for (var args_i = start; args_i < args.length; args_i++) {
+      var name = args[args_i]
+      var val = raw_get(name)
+      ret.push(val);
+    } 
+    return ret;
+  } 
   var commands = {
     add: function (args) {
       return args[1] + args[2];
     },
-    say: function (arg) {console.log(arg[1]); return arg[1]},
-    alert: function (arg) {alert(arg[1])},
-    "goto":function (args) {
-      var name = args[1];
-      goto_cmd(name, args.slice(2));
-    },
-    "goto2":function (args) {
-      var name = args[0];
-      goto_cmd(name, args.slice(1));
-    },
-    call: function (args) {
-      var name = args[1]
-      call_cmd(name, args.slice(2))
-    },
-    call2: function (args) {
-      var name = args[0]
-      call_cmd(name, args.slice(1))
+    say: function (args) {
+      var vals = get_args(args, 1)
+      console.log.apply(console, vals);
+      return vals[0]
     },
     ity: function (args) {
-      return args[1]
-    }, 
-    sets: function (args) {
-      values = args[0];
-      var args_index = 1;
-      for (var args_index = 1; args_index < values.length; args_index += 1) {
-        var name = args[args_index]
-        var value = values[args_index]
-        scope[name] = value;
-      }
-    },
-    ret_val: function (args) {return ret_val},
-    fn: function (args) {
-      var len = args.length
+      var vals = get_args(args, 1)
+      return vals[0]
+    }, "set": function (args) {
+      debugger
       var name = args[1] 
-      fn_map[name] = code.length - 1
-      for (var fn_i = 2; fn_i < len; fn_i++) {
-        code.push(args[fn_i])
+      if (args.length == 3) {
+        ret_val = raw_get(args[2])
       }
-      code.push(return_cmd)
-      i += (len - 3)
-      return i
-    },
-    "return": function (args) {
-      //pop stack  
+      scope[name] = ret_val
+      return ret_val;
+    }, "get": function () {
+    
+    }, "return": function () {
       i = i_stack.pop()
-      scope = scope.parent_scope; 
-      if (!scope) {
+      if (i === undefined) {
         i = code.length; //break!
       }
       return ret_val
-    },
-    "set": function (args) {
-      name = args[1] 
-      scope[name] = ret_val
-      return ret_val;
-    },
-    "push_stack": function () {
-    },
-    "get": function (args) {
-      if (args.length == 2) {
-        var name = args[1]
-        return scope[name]
-      } else {
-        var name = args[2]
-        var scoped_name = args[1]
-        var my_scope = scop[scoped_name]
-        return my_scope[name]
-      }
-    },
-    "get_parent": function (args) {
-      name = args[1]
-      return scope.parent_scope[name]
     }
-  }
 
-  var line = [];
-  var i = 0;
-  var i_stack = [];
-  var scope = {};
-  var fn_map = {}
-  var ret_val
+  }
+  
   var eval_line = function () {
     var my_func = line[0];
     var my_real_func = commands[my_func];
-    if (my_real_func) {
-      ret_val =  my_real_func(line);
-    } else if (is_numeric(my_func)) {
-      i = my_func
-    } else {
-      if (code[i + 1] == return_cmd) {
-        commands.goto2(line)
-      } else {
-        commands.call2(line);
-      }
-    }
+    ret_val =  my_real_func(line);
     i += 1
   }
-  var code;
+
   var evaluate = function (raw_code, scope) {
     scope = scope || {}
     if (is_array(raw_code)) {
@@ -152,8 +114,6 @@ poor_module("drews_lang", function () {
       debugger
       eval_line();
     }
-    console.log("fn_map:")
-    console.log(fn_map)
     return ret_val
   } 
   return evaluate;
