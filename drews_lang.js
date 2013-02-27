03419020beb2ea8461c755119f8a3e5252737efc
drewlesueur <drewalex@gmail.com>
2013-02-27 04:07:21 +0000

    updates

--- drews_lang.js
+++ drews_lang.js
@@ -1,39 +1,17 @@
 // Possible todo:
-// allow to push and pop the code too.
+// scope chains
+// macro
 //implement map
 poor_module("drews_lang", function () {
   var to_string = Object.prototype.toString
   var is_array = function (a) { return to_string.call(a) == '[object Array]' }
   var is_function = function (obj) { return typeof obj === 'function'; }
   var is_string = function (a) { return to_string.call(a) == '[object String]' }
-  var tab_expression = poor_module("drews_express")
+  var drews_expression = poor_module("drews_express")
 
-  var macro_expand = function (code) {
-    return code 
-  }
+  var macro_expand = function (code) { return code }
 
-  var is_numeric = function (numb) {
-    var first_letter = numb[0];
-    return first_letter.search(/\d/) == 0
-  }
   var return_cmd = ["return"];
-
-  var goto_cmd = function (fn_name, args) {
-    scope.args = args;
-    i = fn_map[fn_name];
-  }
-
-  var call_cmd = function (name, args) {
-    //doing "i" and "scope" here
-    i_stack.push(i)
-    var old_scope = scope
-     
-    scope = {parent_scope: old_scope}
-    return ret_val
-    i = fn_map[name]
-    scope.args = args
-  }
-
   var line = [];
   var code;
   var code_stack = [];
@@ -41,14 +19,18 @@ poor_module("drews_lang", function () {
   var i_stack = [];
   var scope = {};
   var scope_stack = {}; //or use scope.parent_scope?
-  var fn_map = {}
+  var global_scope = {}
   var ret_val;
+  var arg_val = {};
   var raw_get = function (name) {
+    //consider strings not being quoted and variables being quoted, or wrapped
+    // consider scope chains?
     if (name[0] == "'") {
       return name.substr(1);
     } else {
       return scope[name];
     }
+    // parent scope etc?
   }
   var get_args = function (args, start) {
     ret = [];
@@ -62,40 +44,82 @@ poor_module("drews_lang", function () {
   var commands = {
     add: function (args) {
       return args[1] + args[2];
-    },
-    say: function (args) {
+    }, say: function (args) {
       var vals = get_args(args, 1)
       console.log.apply(console, vals);
       return vals[0]
-    },
-    ity: function (args) {
-      var vals = get_args(args, 1)
+    }, ity: function (args) {
+      var vals = raw_get(args[0])
       return vals[0]
+    }, "set_lit": function (args) {
+      var name = args[1]
+      scope[name] = args[2]
     }, "set": function (args) {
-      debugger
       var name = args[1] 
       if (args.length == 3) {
         ret_val = raw_get(args[2])
       }
       scope[name] = ret_val
       return ret_val;
-    }, "get": function () {
-    
+    }, "set_global": function (args) {
+      var name = args[0]
+      var ret_val = raw_get(args[2]) 
+      global_scope[name] = ret_val;
+      return ret_val
+    }, "get_global": function (args) {
+      var name = args[1];
+      return global_scope[name];
+    }, "get": function (args) {
+      var name = args[1]  
+      return raw_get(name)
+    }, "arg": function (args) {
+      var arg_val =  raw_get(args[1])
     }, "return": function () {
-      i = i_stack.pop()
+      code = code_stack.pop();
+      if (!code) {
+        i = 0;
+        code = [];
+      }
+      i = i_stack.pop();
       if (i === undefined) {
         i = code.length; //break!
       }
       return ret_val
-    }
+    }, "goto": function () {
+      var new_code = raw_get(args[2]);
+      var new_scope = raw_get(args[3])
+      i = args[1]
+      if (new_code) { code = new_code }
+      if (new_scope) { scope = new_scope }
+      return ret_val;
+    }, "call": function () {
+      var new_i = args[1]
+      var new_code = raw_get(args[2]);
+      var new_scope = raw_get(args[3])
+      i_stack.push(i);
+      i = new_i;
+      code_stack.push(code);
+      if (new_code) { code = new_code }
+      if (new_scope) { scope = new_scope }
+      return ret_val
 
+    }, "set_scope": function (args) {
+      var new_scope = raw_get(args[1])
+    }, "push_scope": function () {
+      scope_stack.push(scope)
+      scope = {};
+      return ret_val
+    }, "pop_scope": function () {
+      scope = scope_stack.pop()
+      return ret_val;
+    }
   }
   
   var eval_line = function () {
     var my_func = line[0];
     var my_real_func = commands[my_func];
     ret_val =  my_real_func(line);
-    i += 1
+    i += 1 //consider using linked lists too??
   }
 
   var evaluate = function (raw_code, scope) {
@@ -103,7 +127,7 @@ poor_module("drews_lang", function () {
     if (is_array(raw_code)) {
       code = raw_code
     } else {
-      code = tab_expression(raw_code)
+      code = drews_expression(raw_code)
     }
     code = macro_expand(code) 
     i = 0;
@@ -111,7 +135,6 @@ poor_module("drews_lang", function () {
     while (true) {
       if (i >= code.length) {break}
       line = code[i]
-      debugger
       eval_line();
     }
     return ret_val
